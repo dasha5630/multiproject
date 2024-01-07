5189a85e36266d1e767bc200a2eb08adee8bdfc9
dasha5630 <dasha5630@yandex.ru>
2024-01-08 01:46:04 +0300

    add new switch format unused vars deleted

--- src/main/java/com/tdv/tech/multiproject/TelegramBot.java
+++ src/main/java/com/tdv/tech/multiproject/TelegramBot.java
@@ -1,21 +1,21 @@
 package com.tdv.tech.multiproject;
 
 import com.tdv.tech.multiproject.config.BotConfig;
-import com.tdv.tech.multiproject.model.CurrencyModel;
 import com.tdv.tech.multiproject.service.CurrencyService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.IOException;
-
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
     private final BotConfig botConfig;
+    public static Logger LOG = LoggerFactory.getLogger(TelegramBot.class);
 
     @Override
     public String getBotUsername() {
@@ -29,32 +29,21 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        CurrencyModel currencyModel = new CurrencyModel();
-        String currency = "";
-
         if(update.hasMessage() && update.getMessage().hasText()){
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
             switch (messageText){
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                case "/currency_list":
-                    currencyList(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                case "/help":
-                    help(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
+                case "/start" -> startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                case "/currency_list" -> currencyList(chatId);
+                case "/help" -> help(chatId);
+                default -> {
                     try {
-                        currency = CurrencyService.getCurrencyRate(messageText);
-
+                        sendMessage(chatId, CurrencyService.getCurrencyRate(messageText));
                     } catch (Exception e) {
                         sendMessage(chatId, "того что вы ищете нет");
                         throw new RuntimeException(e);
                     }
-                    sendMessage(chatId, currency);
+                }
             }
         }
 
@@ -68,56 +57,58 @@ public class TelegramBot extends TelegramLongPollingBot {
         sendMessage(chatId, answer);
     }
 
-    private void help(Long chatId, String name) {
+    private void help(Long chatId) {
         String answer = "/currency_list command";
 
         sendMessage(chatId, answer);
     }
 
-    private void currencyList(Long chatId, String name) {
-        String answer = "                AUD\n" +
-                "                AZN\n" +
-                "                GBP\n" +
-                "                AMD\n" +
-                "                BYN\n" +
-                "                BGN\n" +
-                "                BRL\n" +
-                "                HUF\n" +
-                "                VND\n" +
-                "                HKD\n" +
-                "                GEL\n" +
-                "                DKK\n" +
-                "                AED\n" +
-                "                USD\n" +
-                "                EUR\n" +
-                "                EGP\n" +
-                "                INR\n" +
-                "                IDR\n" +
-                "                KZT\n" +
-                "                CAD\n" +
-                "                QAR\n" +
-                "                KGS\n" +
-                "                CNY\n" +
-                "                MDL\n" +
-                "                NZD\n" +
-                "                NOK\n" +
-                "                PLN\n" +
-                "                RON\n" +
-                "                XDR\n" +
-                "                SGD\n" +
-                "                TJS\n" +
-                "                THB\n" +
-                "                TRY\n" +
-                "                TMT\n" +
-                "                UZS\n" +
-                "                UAH\n" +
-                "                CZK\n" +
-                "                SEK\n" +
-                "                CHF\n" +
-                "                RSD\n" +
-                "                ZAR\n" +
-                "                KRW\n" +
-                "                JPY";
+    private void currencyList(Long chatId) {
+        String answer = """
+                                AUD
+                                AZN
+                                GBP
+                                AMD
+                                BYN
+                                BGN
+                                BRL
+                                HUF
+                                VND
+                                HKD
+                                GEL
+                                DKK
+                                AED
+                                USD
+                                EUR
+                                EGP
+                                INR
+                                IDR
+                                KZT
+                                CAD
+                                QAR
+                                KGS
+                                CNY
+                                MDL
+                                NZD
+                                NOK
+                                PLN
+                                RON
+                                XDR
+                                SGD
+                                TJS
+                                THB
+                                TRY
+                                TMT
+                                UZS
+                                UAH
+                                CZK
+                                SEK
+                                CHF
+                                RSD
+                                ZAR
+                                KRW
+                                JPY\
+                """;
         sendMessage(chatId, answer);
     }
 
@@ -128,7 +119,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
-
+            LOG.error("Telegram api error" + e);
         }
     }
 }
