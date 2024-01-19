79fa92072717fcb2f6f5d12347dd37e8056b08fb
dasha5630 <dasha5630@yandex.ru>
2024-01-19 16:50:56 +0300

    fix SecurityConfig allow to do post request be disabling csrf

--- src/main/java/com/tdv/tech/multiproject/config/SecurityConfig.java
+++ src/main/java/com/tdv/tech/multiproject/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.tdv.tech.multiproject.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,11 +12,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterConfig(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
-        });
-       // http.formLogin(Customizer.withDefaults());
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
